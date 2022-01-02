1b7dd6382e180fec909f0ed6cbe0a774ec68956c
Christian Wirth <christian.wirth@oracle.com>
2022-01-02 17:01:31 +0100

    actually logging in and creating the session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # log user in
+      reset_session
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
