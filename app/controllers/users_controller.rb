d33f37d6861c8222e1d02ca504613403490f1289
Christian Wirth <christian.wirth@oracle.com>
2022-01-05 10:07:52 +0100

    add a post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,15 +52,6 @@ class UsersController < ApplicationController
 
     # Before filters
 
-    # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-        store_location # for friendly forwarding
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-
     #Confirms the correct user.
     def correct_user
       @user = User.find(params[:id])
