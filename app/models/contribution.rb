7cb9a73d7a1b474d324247cbfdb528e722e15a78
Christian Wirth <christian.wirth@oracle.com>
2022-02-12 22:37:48 +0100

    translate some of the error messages

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -6,9 +6,9 @@ class Contribution < ApplicationRecord
   has_one_attached :image
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
-  validates :content, presence: true, length: { maximum: 140 }
-  validates :title, presence: true, length: { maximum: 255 }
+  validates :content, presence: { message: "- Beschreibung ist ein Pflichtfeld" }, length: { maximum: 140 }
+  validates :title, presence: { message: "- Titel ist ein Pflichtfeld" }, length: { maximum: 255 }
   validates :image, 
-      content_type: { in: %w[image/jpeg image/gif image/png], message: "must be a valid image format" },
-      size: { less_than: 5.megabytes, message: "should be less than 5MB" }
+      content_type: { in: %w[image/jpeg image/gif image/png], message: "gültiges Bildformat gefordert (JPG, GIF, PNG)" },
+      size: { less_than: 5.megabytes, message: "muss kleiner als 5MB sein" }
 end
