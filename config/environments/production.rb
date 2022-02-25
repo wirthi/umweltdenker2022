c555677ca811c5e1bfe96701ffcbcd6c50593624
Christian Wirth <christian.wirth@oracle.com>
2022-02-25 20:22:26 +0100

    update paths

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.default_url_options = { :host => 'ppoe-umweltdenker-2022.herokuapp.com' }  
+  config.action_mailer.default_url_options = { :host => 'ppoe-umweltdenker.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp  
   config.action_mailer.perform_deliveries = true  
   config.action_mailer.raise_delivery_errors = true  
@@ -74,7 +74,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {  
     address: "smtp.gmail.com",
     port: 587,
-    domain: "ppoe-umweltdenker-2022.herokuapp.com",
+    domain: "ppoe-umweltdenker.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
