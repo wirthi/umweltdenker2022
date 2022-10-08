460ef1a2f72f0ec61ce9e4726244530dc7af6a8d
Christian Wirth <christian.wirth@oracle.com>
2022-10-08 14:30:27 +0200

    reset railsTestTruffleruby.sh to workaround (no clue why I need that again)

--- railsTestTruffleruby.sh
+++ railsTestTruffleruby.sh
@@ -2,7 +2,7 @@
 
 #required as TruffleRuby currently crashes, parallel testing not supported due to bug in Hash implementation
 
-#export PARALLEL_WORKERS='1'
-#export MT_CPU='1'
+export PARALLEL_WORKERS='1'
+export MT_CPU='1'
 rails test
 
